eefb24e17de5bd07d8cf97c330887fa9932b406e
Alexey Stepanov <stepales1111@gmail.com>
2021-12-19 01:29:40 +0300

    add canvas and table

--- main.js
+++ main.js
@@ -15,3 +15,33 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";
 }
+
+
+
+// canvas
+	// set up initial variables
+	var c = document.getElementById("canvas");
+	var ctx = c.getContext("2d");
+ 
+  ctx.beginPath();
+  ctx.moveTo(200,0);
+  ctx.lineTo(100,300);
+  ctx.stroke();
+
+function drawCircle(x, y, color){
+  ctx.beginPath();
+  ctx.arc(x,y,10,0,2*Math.PI);
+  ctx.fillStyle=color;
+  ctx.fill();
+}
+var x = 0;
+setInterval(function(){ 
+  ctx.clearRect(0,0,400,400);
+  drawCircle(x%300, 85, 'blue');
+  drawCircle((x+20)%300, 55, 'red');
+  drawCircle((x+40)%300, 110, 'yellow');
+  drawCircle((x+60)%300, 25, 'green');
+  x++;
+}, 20);
+
+
