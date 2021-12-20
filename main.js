90e257f24a8bbb9af796de6628b69aaf3abc03b2
Alexey Stepanov <stepales1111@gmail.com>
2021-12-20 20:42:28 +0300

    change slider and style

--- main.js
+++ main.js
@@ -48,3 +48,31 @@ setInterval(function(){
 $('.toggle-menu-btn').click((event) => {
   $('.nav-link1').slideToggle('slow', () => {});
 });
+
+
+function slideChange(sliderID, slideDirection) {
+  var currentSlider = document.getElementById(sliderID);
+  var slides = currentSlider.getElementsByClassName('slider_slide');
+  for (var i = 0; i < slides.length; i++) {
+      if (slides[i].classList.contains('slider_slide_active')) {
+          slides[i].classList.remove('slider_slide_active');
+          slides[i].classList.add('slider_slide_inactive');
+          if (slideDirection == 'left') {
+              if (i == 0) {
+                  i = slides.length - 1;
+              } else {
+                  i--;
+              }
+          } else {
+              if (i == slides.length - 1) {
+                  i = 0;
+              } else {
+                  i++;
+              }
+          }
+          slides[i].classList.remove('slider_slide_inactive');
+          slides[i].classList.add('slider_slide_active');
+          break;
+      }
+  }
+}
